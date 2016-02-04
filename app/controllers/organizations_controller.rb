3f098275d780159850a6547f29480ffde83132cf
Igor Marques <igormarquessilva@gmail.com>
2016-02-03 21:22:27 -0300

    Add (public) repos list to PRs view

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -6,5 +6,10 @@ class OrganizationsController < ApplicationController
 
   def pull_requests
     @org = params[:id]
+
+    # THIS IS ONLY GETTING PUBLIC REPOS
+    @repos = @client.repos(@org).map{|x| x[:name]}
+
+    #@client.repos is able to get even private repos
   end
 end
