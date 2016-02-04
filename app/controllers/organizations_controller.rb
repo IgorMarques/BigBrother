3d02f6b093bda714f1becd891554993df35344f7
Igor Marques <igormarquessilva@gmail.com>
2016-02-03 22:18:19 -0300

    Add pr listing for a org

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -8,8 +8,30 @@ class OrganizationsController < ApplicationController
     @org = params[:id]
 
     # THIS IS ONLY GETTING PUBLIC REPOS
-    @repos = @client.repos(@org).map{|x| x[:name]}
+    @repos = @client.repos(@org).map do |r|
+      {
+        name: r[:name],
+        pull_requests: get_pull_requests( r[:name])
+      }
+    end
 
     #@client.repos is able to get even private repos
+
+    @repos << {name: "Hiroshima", pull_requests: get_pull_requests("Hiroshima")}
+
+    # raise Exception.new @bonus
+  end
+
+  private
+
+  def get_pull_requests(repo)
+    @client.pull_requests("#{@org}/#{repo}")
+    .map do |pr|
+      build_pr(pr)
+    end
+  end
+
+  def build_pr(pull_request)
+    pull_request.to_hash.slice(:number, :html_url, :title)
   end
 end
